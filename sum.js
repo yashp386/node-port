7425950029af427efab1665def69be1bc6119acb
yashp386 <yashp30386@gmail.com>
2024-09-22 21:16:41 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,12 +1,13 @@
 // modules are protected byv default they are protected their functions and variables from leaking
 // to use modules in node js u have to explicitly export it 
-module.exports =calculatesum;   //exported the specified module
+  //exported the specified module
 console.log("sum ececuted");
 
 let a=10,b=10;
 
 function calculatesum(a,b){
-    const sum =a+b;
+    let sum =a+b;
     console.log(sum);
 }
 
+module.exports =calculatesum; 
